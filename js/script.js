18fc1b748e0da673d02a6d3829ddb8b849102aa3
Amegakm <109219902+Amegakm@users.noreply.github.com>
2026-03-22 23:19:07 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+// Navigation handler
+function goToPage(page){
+window.location.href = page;
+}
+
+// Future: you can add
+// - Firebase
+// - Form handling
+// - Animations
+// - Live updates logic
+
+console.log("Samashti site loaded");
 
